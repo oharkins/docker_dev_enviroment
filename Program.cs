78949e299d32cbce067aeeef957e1a50ab0d90ca
Odis Harkins <odisjamesharkins@gmail.com>
2022-10-15 02:19:31 +0000

    Added More Data

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
 ﻿Console.WriteLine("Hello, World!");
+
+var testthis = new testthis();
+testthis.getThis();
+public class testthis
+{
+    public void getThis()
+    {
+        Console.WriteLine("Got That Stuff");
+    }
+}
